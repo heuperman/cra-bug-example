42934f967783e88ed851de3266615c7a577ac661
Cornelis Heuperman <cheuperman@gmail.com>
2022-06-01 16:49:21 +0200

    Add dev dependency to test file
    Add a dev dependency to a test file to show the build will try to
    include the test file and fail.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { setupWorker } from "msw";
 
-test('renders learn react link', () => {
+const worker = setupWorker();
+
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
